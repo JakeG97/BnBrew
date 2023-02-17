33b4e060d3895d21d3bc9c5ccbc90441ff38e8db
Jake Gularte <jakegularte@Jakes-MacBook-Air.local>
2023-02-16 16:00:53 -0800

    fix

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const express = require("express");
+const { check } = require("express-validator");
+
+const { handleValidationErrors } = require("../../utils/validation");
+const { setTokenCookie } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
 const router = express.Router();
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
 
 const validateLogin = [
     check('credential')
