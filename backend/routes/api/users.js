51ff5d1a77652120ee545b29df8f35a8fccd22fc
Jake Gularte <jakegularte@Jakes-MacBook-Air.local>
2023-02-21 14:42:51 -0800

    spots route created

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -31,7 +31,7 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { firstName, lastName, email, username, password, } = req.body;
+      const { firstName, lastName, email, username, password } = req.body;
       const user = await User.signup({ firstName, lastName, email, username, password });
   
       await setTokenCookie(res, user);
