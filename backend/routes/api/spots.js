569acfdb0469fca8fb4b3b165c036ae91a686d2d
Jake Gularte <jakegularte@Jakes-MacBook-Air.local>
2023-02-22 01:18:17 -0800

    create a spot done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -95,6 +95,47 @@ router.get('/:spotId', async (req, res) => {
 });
 
 
-// 
+// create a spot
+router.post('/', async (req, res) => {
+    const user = req.user
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+
+    try {
+        const newSpot = await Spot.create({
+            ownerId: user.id,
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price
+        });
+
+        return res.json(newSpot)
+    } catch {
+        res.statusCode = 400;
+        return res.json({
+            message: "Validation Error",
+            statusCode: res.statusCode,
+            errors: {
+                "address": "Street address is required",
+                "city": "City is required",
+                "state": "State is required",
+                "country": "Country is required",
+                "lat": "Latitude is not valid",
+                "lng": "Longitude is not valid",
+                "name": "Name must be less than 50 characters",
+                "description": "Description is required",
+                "price": "Price per day is required"
+            }
+        });
+
+    };
+});
+
+
 
 module.exports = router;
