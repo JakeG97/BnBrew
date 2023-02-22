9a5a6d985934af99dec87af9668c6bf75a90fc7e
Jake Gularte <jakegularte@Jakes-MacBook-Air.local>
2023-02-22 00:47:54 -0800

    spots owned by current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -13,5 +13,44 @@ router.get('/', async (req, res) => {
     res.status(200).json({Spots: spots});
 });
 
+// get spots owned by current User
+router.get('/current', requireAuth, async (req, res) => {
+    const ownerId = req.user.id
+
+    const spots = await Spot.findAll({
+        where: {
+            ownerId: ownerId
+        },
+
+    });
+
+    let avgRating;
+    for (let i = 0; i < spots.length; i++) {
+        const reviewCount = await Review.count({ where: { spotId: spots[i].id } })
+        const sumOfStars = await Review.sum('stars', {
+            where: { spotId: spots[i].id }
+        });
+
+        if (!sumOfStars) {
+            avgRating = 0;
+        } else {
+            avgRating = (sumOfStars / reviewCount).toFixed(1);
+        }
+
+        spots[i].avgRating = avgRating;
+
+        const spotImage = await SpotImage.findOne({
+            where: { spotId: spots[i].id },
+            attributes: ['id', 'url', 'preview']
+        });
+
+        if (spotImage) spots[i].previewImage = spotImage.url;
+        else spots[i].previewImage = 'No Image Available'
+
+    }
+
+    return res.json({ Spots: spots })
+});
+
 
 module.exports = router;
