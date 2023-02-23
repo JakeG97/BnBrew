5b2518972918e1c2ced819cf16f43f2b5e8e43b2
Jake Gularte <jakegularte@Jakes-MacBook-Air.local>
2023-02-23 14:18:33 -0800

    delete spot images complete

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -3,7 +3,26 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { SpotImage } = require('../../db/models');
 const router = express.Router();
 
+// delete a spot image
 
+router.delete("/:imageId", requireAuth, async (req, res) => {
+    const { imageId } = req.params;
+    const findImage = await ReviewImage.findByPk(imageId);
+  
+    if (!findImage) {
+      res.status(404);
+      return res.json({
+        message: "Image couldn't be found",
+        statusCode: 404,
+      });
+    }
+  
+    await findImage.destroy();
+    res.json({
+      message: "Successfully deleted",
+      statusCode: 200,
+    });
+  });
 
 
 module.exports = router;
