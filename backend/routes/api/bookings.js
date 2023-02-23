5f37a177fcc96ea5a311607f917a08437c31ec2f
Jake Gularte <jakegularte@Jakes-MacBook-Air.local>
2023-02-23 12:55:42 -0800

    get all bookings complete

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,9 +1,50 @@
 const express = require('express');
-const { Booking, User, Spot, SpotImage } = require("../../db/models");
-const { requireAuth } = require("../../utils/auth");
+const { Booking, User, Spot, SpotImage } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
-const sequelize = require("sequelize");
-const booking = require("../../db/models/booking");
+const sequelize = require('sequelize');
+const booking = require('../../db/models/booking');
+
+
+// get all of the current user's bookings
+router.get('/current', requireAuth, async (req, res) => {
+    const userId = req.user.id
+    const currBookings = await Booking.findAll({
+        where: {
+            userId: userId
+        },
+        include: [
+            {
+                model: Spot,
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                },
+            },
+
+        ],
+    });
+
+    for (let i = 0; i < currBookings.length; i++) {
+        let bookingsPreview = currBookings[i].toJSON();
+
+        let bookingImg = bookingsPreview.Spot.id;
+        const spotImg = await SpotImage.findOne({
+            where: {
+                spotId: bookingImg,
+                preview: true
+            }
+        });
+
+        if (spotImg) {
+            bookingsPreview.Spot.previewImage = spotImg.url
+        } else {
+            bookingsPreview.Spot.previewImage = 'No image available '
+        }
+        currBookings[i] = bookingsPreview;
+    };
+
+    return res.json({ Bookings: currBookings })
+});
 
 
 
@@ -15,6 +56,4 @@ const booking = require("../../db/models/booking");
 
 
 
-
-
-modeule.exports = router;
+module.exports = router;
