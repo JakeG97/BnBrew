ec462c160e695665208f7787222483ee85fded32
Jake Gularte <jakegularte@Jakes-MacBook-Air.local>
2023-02-23 12:38:24 -0800

    bookings router set up

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { Booking, User, Spot, SpotImage } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
+const router = express.Router();
+const sequelize = require("sequelize");
+const booking = require("../../db/models/booking");
+
+
+
+
+
+
+
+
+
+
+
+
+
+modeule.exports = router;
