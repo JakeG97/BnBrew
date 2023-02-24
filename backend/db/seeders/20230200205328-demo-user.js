89c9696da9171050ff68b42bb60abf6b9c30235b
Jake Gularte <jakegularte@Jakes-MacBook-Air.local>
2023-02-24 14:39:38 -0800

    seeder syntax fix

--- backend/db/seeders/20230200205328-demo-user.js
+++ backend/db/seeders/20230200205328-demo-user.js
@@ -5,10 +5,10 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-options.tableName = 'Users';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
         firstName: 'Billy',
