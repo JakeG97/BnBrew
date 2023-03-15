689a8edeff309d0fd960b994f407de3441c15807
JakeG97 <jgularteee@gmail.com>
2023-03-15 14:01:11 -0700

    Review syntax fixed to properly render on spot details page

--- frontend/src/components/ReviewForm/index.js
+++ frontend/src/components/ReviewForm/index.js
@@ -2,24 +2,19 @@ import "./ReviewForm.css"
 
 export default function Reviews({review}){
 
-    if (review.stars === 0) review.stars = "No Revies Yet"
-
     return (
         <div>
-            {review.stars}
-            <div>
-                <h2 className="review-div">Reviews</h2>
-                {Object.values(review).map((review) => (
-                    <div className="star-block" key={review.id}>
-                        <div className="name">Review by: {review.User?.firstName}
-                        </div>
-                            <div className="name">{review.review}
-                            </div>
-                                <div id="star" className="fas fa-sold fa-star"> {review.stars}
-                                </div>    
+            <h2 className="review-div">Reviews</h2>
+            {Object.values(review).map((review) => (
+                <div className="star-block" key={review.id}>
+                    <div className="name">Review by: {review.User?.firstName}
                     </div>
-                ))}
-            </div>
+                        <div className="name">{review.review}
+                        </div>
+                            <div id="star" className="fas fa-sold fa-star"> {review.stars}
+                            </div>    
+                </div>
+            ))}
         </div>
     );
 }
