19cee313305b929ae147d6e6b73a26012a789b30
JakeG97 <jgularteee@gmail.com>
2023-03-19 18:24:38 -0700

    seeder changes

--- backend/db/seeders/20230201063709-demo-spot.js
+++ backend/db/seeders/20230201063709-demo-spot.js
@@ -43,7 +43,62 @@ module.exports = {
       name: "Fieldwork Brewing Company",
       description: "Brewpub with a variety of seasonal house beer blends, in an industrial setting.",
       price: 12.00
-    }
+    },{
+      ownerId: 4,
+      address: "1885 MacArthur DR",
+      city: "Tracy",
+      state: "California",
+      country: "USA",
+      lat: 122.3024,
+      lng: 37.8812,
+      name: "Morgan Territory Brewing",
+      description: "Morgan Territory Brewing is taking on the Wild West. Named after a historic ranch located on the “morning side” of Mount Diablo, Morgan Territory Brewing is dedicated to the rich, independent character of the early pioneers that made California great. They are committed to producing bold and diverse, high-quality craft beers, and continuing their award-winning legacy.",
+      price: 6.00
+  },{
+    ownerId: 1,
+    address: "322 Bellevue Ave",
+    city: "Santa Rosa",
+    state: "California",
+    country: "USA",
+    lat: 122.3035,
+    lng: 37.8862,
+    name: "HenHouse Brewing Company",
+    description: "HenHouse Brewing Company makes some of the best tasting beers that you can get and each and every one of them has a story to tell... literally, look on the side of the can!",
+    price: 5.00
+  },{
+    ownerId: 2,
+    address: "380 S Rock Blvd",
+    city: "Sparks",
+    state: "Nevada",
+    country: "USA",
+    lat: 123.3024,
+    lng: 34.8812,
+    name: "Revision Brewing Company",
+    description: "Revision Brewing Company is a craft beer brewery and taproom located in a 30,500 sq. ft. building just off Interstate 80 in industrial Sparks, Nevada. Enjoy a range of available beer styles in your choice of atmosphere—our temperature-regulated taproom, or the warehouse side where you can play foosball, bag toss, giant Connect Four, and pool with the bustle of the brewery operations happening all around you. ",
+    price: 6.00
+  },{
+    ownerId: 3,
+    address: "220 S. Raymond Ave",
+    city: "Pasadena",
+    state: "California",
+    country: "USA",
+    lat: 123.4024,
+    lng: 34.5812,
+    name: "Stone Brewing Tap Room",
+    description: "Stone Brewing Tap Room - Pasadena is a tasting room and retail store with patio space in the Del Mar Metro Station complex (formerly the historic Santa Fe train depot). This tap room is right next to the Los Angeles Metro Rail Gold Line in the heart of downtown Pasadena, within easy walking distance of Old Town Pasadena. Our unique tasting experience features numerous rotating taps of Stone Brewing beers, including year-round beers, special and one-time releases, and periodic archived vintages. Purchase growler fills of fresh Stone beer, kegs, bottles, and Stone Brewing merchandise and gifts, including t-shirts, hats, glassware, dog treats, and more.",
+    price: 10.00
+  },{
+    ownerId: 2,
+    address: "725 4th Street",
+    city: "Santa Rosa",
+    state: "California",
+    country: "USA",
+    lat: 131.4024,
+    lng: 34.6212,
+    name: "Russian River Brewing Company",
+    description: "Famous for their hoppy, crisp tasting beers, Russian River makes you upset you tried their brew.. because no other drink will taste just as good.",
+    price: 13.00
+  }
     ], options)
   },
 
@@ -51,7 +106,10 @@ module.exports = {
     options.tableName = "Spots";
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      name : { [Op.in]: ["Lagunitas Brewing Company", "Boneyard Pub", "Fieldwork Brewing Company"] }
+      name : { [Op.in]: ["Lagunitas Brewing Company", "Boneyard Pub", "Fieldwork Brewing Company",
+                        "Morgan Territory Brewing", "HenHouse Brewing Company", "Revision Brewing Company",
+                        "Stone Brewing Tap Room", "Russian River Brewing Company"
+    ] }
     }, {})
   }
 };
--- backend/db/seeders/20230221065250-demo-review.js
+++ backend/db/seeders/20230221065250-demo-review.js
@@ -27,6 +27,36 @@ module.exports = {
         userId: 2,
         review: "If you like Hazy IPA's this place is awesome... I hate Hazy IPA's..",
         stars: 2,
+      },
+      {
+        spotId: 4,
+        userId: 3,
+        review: "The town really doesn't have much to offer.. but if there's one thing it does, it's this amazing brewery!",
+        stars: 5,
+      },
+      {
+        spotId: 5,
+        userId: 1,
+        review: "I have yet to have a beer from them that dissapoints so I had to check out the brewery and I was not dissapointed.",
+        stars: 5,
+      },
+      {
+        spotId: 6,
+        userId: 1,
+        review: "I think the palce was really nice but man there beer is strong, I can't really remember",
+        stars: 3,
+      },
+      {
+        spotId: 7,
+        userId: 2,
+        review: "Stone brewing really put's the stone cold chill in their beer, was so cold my mouth went numb!",
+        stars: 4,
+      },
+      {
+        spotId: 8,
+        userId: 4,
+        review: "I had heard all the hype and they did not dissapoint!",
+        stars: 5,
       }
     ], options);
   },
